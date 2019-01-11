833bb7a78d3d97088b3401189b04fdd1aea40101
Casual Gary <gamemaster@nil.gen.nz>
2019-01-11 16:18:17 +1300

    you, me , toby, hugh - lfd2 , it will rock the world!

--- app/src/main/java/nz/gen/nil/drpoopoo/RubbishTruckSimulatorActivity.kt
+++ app/src/main/java/nz/gen/nil/drpoopoo/RubbishTruckSimulatorActivity.kt
@@ -2,11 +2,30 @@ package nz.gen.nil.drpoopoo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import java.util.*
+
 
 class RubbishTruckSimulatorActivity : AppCompatActivity() {
+    private val rando = Random()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rubbish_truck_simulator)
+
+        rando.setSeed(Date().time)
+
+        val noun = rollForString(R.array.nouns)
+        val adjective = rollForString(R.array.adjectives)
+        val reason = rollForString(R.array.reasons)
+
+        val textView = findViewById<TextView>(R.id.somethingoriginal)
+        textView.setText(resources.getString(R.string.dana_scully, noun, adjective, reason))
+    }
+
+    private fun rollForString(id: Int): String {
+        val strings = resources.getStringArray(id)
+        val index = rando.nextInt(strings.size)
+        return strings[index]
     }
 }
